bf7a3a7e2c529c6be9e2bac380549e3897560194
Eriquy Jhordan <eriquy@gmail.com>
2021-01-22 16:38:56 -0300

    appointments routes

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import AppointmentRepository from '../Repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentRouter = Router();
+
+appointmentRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentRepository);
+  const appointments = await appointmentsRepository.find();
+  return response.json(appointments);
+});
+
+appointmentRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+    const paserDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService();
+    const appointment = await createAppointment.execute({
+      date: paserDate,
+      provider,
+    });
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.mensage });
+  }
+});
+
+export default appointmentRouter;
