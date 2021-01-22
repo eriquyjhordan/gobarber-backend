6199cc33acad41484e5d1005022e273816be2214
Eriquy Jhordan <eriquy@gmail.com>
2021-01-22 17:08:28 -0300

    alteracao das migrations de users e appointments e alteracao do campo provider para provider_id com o tipo uuid

--- src/database/migrations/1610023167910-CreateUsers.ts
+++ src/database/migrations/1610023167910-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1610023167910 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
---
+++ src/database/migrations/1611344675306-AlterProviderFildToProviderId.ts
@@ -0,0 +1,44 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AlterProviderFildToProviderId1611344675306
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        referencedTableName: 'users',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        name: 'provider_foreign_key',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'provider_foreign_key');
+    await queryRunner.dropColumn('appointments', 'provider_id');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      }),
+    );
+  }
+}
