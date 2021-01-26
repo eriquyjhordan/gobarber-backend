05417e43afb9de528713fa6a36559c7c20a82ab6
Eriquy Jhordan <eriquy@gmail.com>
2021-01-26 11:06:31 -0300

    rota de autenticacao

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentRoutes from './appointments.routes';
 import usersRouter from './users.routes';
+import authenticationRoutes from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appoitments', appointmentRoutes);
 routes.use('/users', usersRouter);
+routes.use('/sessions', authenticationRoutes);
 
 export default routes;
